825db10e88e057972923b406586723535c8ec4fb
Harrison Phan <harrisonphan5@gmail.com>
2022-02-26 20:56:02 +0700

    day 5, problem 1

--- src/main/java/com/example/Solution.java
+++ src/main/java/com/example/Solution.java
@@ -8,6 +8,9 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
+import com.example.Result.SinglyLinkedList;
+import com.example.Result.SinglyLinkedListNode;
+
 class Result {
 
     public static void plusMinus(List<Integer> arr) {
@@ -194,9 +197,8 @@ class Result {
         for (int i = q.size() - 1; i >= 0; i--) {
             int expected_val = i + 1;
             if (q.get(i) == expected_val) {
-                // fit
-            }
-            else if (i >= 1 && q.get(i - 1) == expected_val) {
+                // right position
+            } else if (i >= 1 && q.get(i - 1) == expected_val) {
                 bribes++;
                 int temp = q.get(i);
                 q.set(i, q.get(i - 1));
@@ -215,29 +217,120 @@ class Result {
         System.out.println(bribes);
     }
 
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter)
+            throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        SinglyLinkedList root = new SinglyLinkedList();
+
+        SinglyLinkedListNode nodeHead1 = head1;
+        SinglyLinkedListNode nodeHead2 = head2;
+        while (nodeHead1 != null && nodeHead2 != null) {
+            if (nodeHead1.data < nodeHead2.data) {
+                root.insertNode(nodeHead1.data);
+                nodeHead1 = nodeHead1.next;
+            }
+            else {
+                root.insertNode(nodeHead2.data);
+                nodeHead2 = nodeHead2.next;
+            }
+        }
+        while (nodeHead1 != null) {
+            root.insertNode(nodeHead1.data);
+            nodeHead1 = nodeHead1.next;
+        }
+        while (nodeHead2 != null) {
+            root.insertNode(nodeHead2.data);
+            nodeHead2 = nodeHead2.next;
+        }
+        return root.head;
+    }
 }
 
 public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("test.txt"));
 
-        int t = Integer.parseInt(bufferedReader.readLine().trim());
+        int tests = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        for (int tItr = 0; tItr < t; tItr++) {
-            int n = Integer.parseInt(bufferedReader.readLine().trim());
+        for (int testsItr = 0; testsItr < tests; testsItr++) {
+            SinglyLinkedList llist1 = new SinglyLinkedList();
 
-            String[] qTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+            int llist1Count = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-            List<Integer> q = new ArrayList<>();
+            for (int i = 0; i < llist1Count; i++) {
+                int llist1Item = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-            for (int i = 0; i < n; i++) {
-                int qItem = Integer.parseInt(qTemp[i]);
-                q.add(qItem);
+                llist1.insertNode(llist1Item);
+            }
+          
+          	SinglyLinkedList llist2 = new SinglyLinkedList();
+
+            int llist2Count = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llist2Count; i++) {
+                int llist2Item = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist2.insertNode(llist2Item);
             }
 
-            Result.minimumBribes(q);
+            SinglyLinkedListNode llist3 = Result.mergeLists(llist1.head, llist2.head);
+
+            Result.printSinglyLinkedList(llist3, " ", bufferedWriter);
+            bufferedWriter.newLine();
         }
 
-        bufferedReader.close();
+        bufferedWriter.close();
+
+        scanner.close();
     }
 }
